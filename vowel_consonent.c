c3dd3e11b3b363752dc04ce2be412e891e82643a
mamunur4117 <mamunur4117@gmail.com>
2024-10-14 13:54:09 +0600

    vowel_consonent.c

---
+++ vowel_consonent.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    char C;
+    scanf("%c",&C);
+   
+        if(C=='a')
+        {
+            printf("Vowel");
+        }
+        else if(C=='e')
+        {
+            printf("Vowel");
+        }
+        else if(C=='i')
+        {
+            printf("Vowel");
+        }
+        else if(C=='o')
+        {
+            printf("Vowel");
+        }
+        else if(C=='u')
+        {
+            printf("Vowel");
+        }
+        else
+        {
+            printf("Consonant");
+        } 
+    return 0;
+}
